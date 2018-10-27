5c62280db24368eeabcc05658cde2103b6da1ca6
orzngo <orzlabo@gmail.com>
2018-10-27 23:14:07 +0900

    スキができた

--- src/Chance.ts
+++ src/Chance.ts
@@ -1,9 +1,29 @@
 // 隙
 export class Chance extends g.E {
-    static chances = ["スキ", "隙", "Chance"];
-    static fakes = ["スギ", "ヌキ", "又キ", "好き", "ズキ", "障", "Change", "Charge", "ジャギ", "Chan", "Chace", "除"];
+    static chances:string[][] = [
+        ["スキ", "スギ", "ヌキ", "又キ", "好き", "ズキ"],
+        ["隙", "障", "除"],
+        ["Chance", "Charge", "Change", "Chan"]
+    ];
 
-    constructor(param: g.EParameterObject, public fake: boolean = false) {
+    text:string;
+
+    static getText(fake:boolean):string {
+        if (!fake) {
+            return Chance.chances[0][0];
+        }
+        return Chance.chances[0][g.game.random.get(1, 5)];
+
+    }
+
+    constructor(param: g.EParameterObject, font:g.Font, scene:g.Scene, public fake: boolean = false) {
         super(param);
+
+        const rect = new g.FilledRect({scene, width:80, height:80, cssColor:"white"});
+        this.append(rect);
+        this.text = Chance.getText(fake);
+        const label = new g.Label({scene, font, text:this.text, fontSize:40});
+        this.append(label);
+        label.y = 20;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import {Seikimatsu} from "./enemy/Seikimatsu";
+import {Chance} from "./Chance";
 
 function main(param: g.GameMainParameterObject): void {
     const scene = new g.Scene({game: g.game, assetIds:Seikimatsu.enemies});
     const enemyFactory = new Seikimatsu(scene);
+    const font = new g.DynamicFont({game:g.game, fontFamily:g.FontFamily.Serif, size:40});
     scene.loaded.add(() => {
         // 以下にゲームのロジックを記述します。
         const enemy = enemyFactory.fromLevel(0);
+        const chance = new Chance({scene},font, scene);
 
         enemy.update.add(() => {
             // 以下のコードは毎フレーム実行されます。
@@ -14,6 +17,7 @@ function main(param: g.GameMainParameterObject): void {
             enemy.modified();
         });
         scene.append(enemy);
+        scene.append(chance);
     });
     g.game.pushScene(scene);
 }
