2ca9bdb120761252c4bfea94d5e82e9d96b1a1c5
orzngo <orzlabo@gmail.com>
2018-10-29 10:31:13 +0900

    kenpou

---
+++ src/chance/NorthStarFist.ts
@@ -0,0 +1,42 @@
+import {Chance} from "./Chance";
+import {Enemy} from "../enemy/Enemy";
+
+// Chance Factory
+
+export class NorthStartFist {
+
+    font:g.DynamicFont;
+
+    constructor(public game:g.Game, public scene:g.Scene) {
+        this.font = new g.DynamicFont({game:g.game, fontFamily:g.FontFamily.Serif, size:40});
+    }
+
+    /**
+     * 敵のスキを見つけ出すがたまにフェイントに引っかかる
+     *
+     * @param {Enemy} enemy
+     * @returns {Chance}
+     */
+    getChancesFromEnemy(enemy:Enemy):Chance[] {
+        const chanceNum = this.game.random.get(1, 3+enemy.level);
+
+        const fakeChance = 10 - enemy.level;
+
+        let results:Chance[] = [];
+        for(let i = 0; i < chanceNum; i++) {
+            let isFake = false;
+            if (i > 0) {
+                isFake = this.game.random.get(0,9) >= fakeChance;
+            }
+            const chance = this.getChance(isFake);
+            chance.x = this.game.random.get(0, this.game.width - chance.width);
+            chance.y = this.game.random.get(0, this.game.height - chance.height);
+            results.push(chance);
+        }
+        return results;
+    }
+
+    getChance(isFake:boolean = false):Chance {
+        return new Chance({scene:this.scene}, this.font,isFake);
+    }
+}
