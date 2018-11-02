77781a70a8108d63c7f6f9dfc59c70d263153851
morio_yamada <morio_yamada@dwango.co.jp>
2018-11-02 16:40:13 +0900

    typeofでwindowをチェック

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import {FightScene} from "./scene/FightScene";
 
-declare var window:any;
+declare var window: any;
 
 function main(param: g.GameMainParameterObject): void {
     g.game.vars.isAtsumaru = false;
 
-    if (window && window.RPGAtsumaru) {
+    if (typeof window !== 'undefined' && window.RPGAtsumaru) {
         g.game.vars.isAtsumaru = true;
     }
     g.game.pushScene(new FightScene());
